68a6c3653f9389ae44fbb6200283d5a1e3d8dd0b
Juzantino Junadi <JUZANTINOJ@GMAIL.COM>
2021-12-27 09:59:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ let newNumber = document.getElementById('number-input');
 
 // creating an eventlistener for the submit button
 addNamenum.addEventListener('click', function () {
+
+ //create a div for new info
  let creatediv = document.createElement('div')
  creatediv.classList.add('input');
 
  //display the value inside the input and creates a div in the addContact
  creatediv.innerHTML = `Name: ${newName.value}    Contact Number: ${newNumber.value}`
+
 
  addContacts.appendChild(creatediv);
  // remove the value inside the inputs
@@ -39,3 +42,43 @@ addNamenum.addEventListener('click', function () {
 //  }
 // }
 // Err();
+
+// //Information Class: Represent the information
+// class Information {
+//  constructor(name, number) {
+//   this.name = name;
+//   this.number = number;
+//  }
+// //UI class: handle ui tasks
+// class UI {
+//  static displayInfo() {
+//   const addContacts = document.getElementById('addContacts');
+
+//   const creatediv = document.createElement('div');
+//   creatediv.classList.add('input');
+//  }
+// }
+// //Store class: handles storage
+
+// // Event : display Informations
+
+// //event : add information
+// document.getElementById('container').addEventListener('submit', (e) => {
+//  e.preventDefault();
+ 
+//  //get form values
+//  let newName = document.getElementById('name-input'.value);
+//  let newNumber = document.getElementById('number-input').value;
+
+//  //instatiate info
+//  const newInfo = new newInfo(newName,newNumber);
+// }
+// //add info to UI
+// UI.displayInfo()
+
+//event : remove information
+
+
+
+
+ 
